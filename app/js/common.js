209be9affeaa6a5db69b90a0825bde9ce1837635
topalidinka <topalidinka@gmail.com>
2019-05-17 11:44:23 +0300

    Added login and sign in popups

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,32 @@
-$(function() {
+$(document).ready(function() {
+		
+	$('.popup__open').on('click', function(event) {
+		event.preventDefault();
+		var popup = $(this).attr('href');
+		closePopup('.popup')
+		openPopup(popup);
+	});
 
-	// Custom JS
+	$('.popup__close').on('click', function(event) {
+		event.preventDefault();
+		var popup = $(this).attr('href');
+		closePopup(popup);
+	});
+
+	function openPopup(popup) {
+		$(popup).css({'display': 'block'});
+		$('body').addClass('ohidden');
+	}
+
+	function closePopup(popup) {
+		$(popup).css({'display': 'none'});
+		$('body').removeClass('ohidden');
+	}
+
+	$(document).click(function(event) {
+		if (!$(event.target).closest('.popup__wrapper, .popup__open').length) {
+			closePopup('.popup')
+		}
+	});
 
 });
